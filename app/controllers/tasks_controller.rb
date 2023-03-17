b5d30b49406ef0ed07bc23486f31eeda23c7aef9
Michelle Santiago <mdcsantiago0@gmail.com>
2023-03-18 01:52:39 +0800

    chore: task controller and views for crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,49 @@
 class TasksController < ApplicationController
+    before_action :get_category
+
+    def index
+        @tasks = @category.tasks
+    end
+
+    def new
+        @task = @category.tasks.build
+    end
+
+    def create
+        @task = @category.tasks.build(task_params)
+        if @task.save
+            redirect_to category_tasks_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @task =  @category.tasks.find(params["id"])
+    end
+    
+    def update
+        @task = @category.tasks.find(params["id"])
+        if @task.update(task_params)
+            redirect_to category_tasks_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @task = @category.tasks.find(params["id"])
+        @task.destroy
+        redirect_to category_tasks_path
+    end
+
+    private
+
+    def get_category
+        @category = Category.find(params["category_id"])
+    end
+
+    def task_params
+        params.require(:task).permit(:title,:description,:deadline,:category_id)
+    end
 end
