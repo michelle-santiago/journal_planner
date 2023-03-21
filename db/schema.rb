6657e70bcfccfdc7e35f7255c57a687ab1469231
Michelle Santiago <mdcsantiago0@gmail.com>
2023-03-21 16:46:22 +0800

    chore: add priority and status for tasks migration and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_095746) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_021646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_095746) do
     t.string "description"
     t.datetime "deadline"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "priority"
+    t.string "status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
