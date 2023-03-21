57420f007b7e0d70b3014a045c0e6c79f92a7915
Michelle Santiago <mdcsantiago0@gmail.com>
2023-03-21 22:16:47 +0800

    chore: add task validation in model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :category
+    validates :title, presence: true
+    validates :category_id, presence: true
+    validates :priority, presence: true
+    validates :status, presence: true
 end
