1a2a80038d256d978504a0cb45fe6a5fca625382
Michelle Santiago <mdcsantiago0@gmail.com>
2023-03-22 19:46:09 +0800

    fix: sign in

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,21 +1,35 @@
 class AuthController < ApplicationController
     def sign_in
+        @user = User.new
     end
 
     def sign_up
     end
 
     def new_session
-        if User.valid_sign_in?(user_params)
+        @user = User.find_by_email(user_params[:email])
+        if @user.password == user_params[:password]
+            render json: { token: user.token }, status: 200
         else
-            render json: {not_found: true}, status: 403
+            redirect_to sign_in_path
         end
+        
+        # if User.valid_sign_in?(user_params)
+        # else
+        #     render json: {not_found: true}, status: 403
+        # end
+
+        # if user = User.sign_in(user_params)
+        #     render json: { token: user.token }, status: 200
+        # else
+        #     render json: { not_found: true }, status: 403
+        # end
     end
 
     def new_account
         if (sign_up_params[:password] == sign_up_params[:password_confirmation])
             @user = User.sign_up(user_params)
-            redirects_to categories.path
+            redirects_to categories_path
         else
             render :sign_up, status: :unprocessable_entity
         end
