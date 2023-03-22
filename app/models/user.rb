be101dd23114ef23de1efb885edc324bab6aafa0
Michelle Santiago <mdcsantiago0@gmail.com>
2023-03-22 19:46:31 +0800

    fix: sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
+require 'bcrypt'
 class User < ApplicationRecord
-    include Bcrypt
+    validates :email, uniqueness: {case_sensitive: false}, presence: true
+    validates :password, presence: true
     has_many :categories
     has_many :tasks, through: :categories
 
     after_create :generate_token
 
     def self.sign_up(user_params)
-        password_hash = Password.create(user_params[:password])
+        password_hash = BCrypt::Password.create(user_params[:password])
 
         create(email: user_params[:email], password: password_hash)
     end
@@ -15,7 +17,7 @@ class User < ApplicationRecord
         user = find_by(email: user_params[:email])
         
         if user.present?
-        valid_password = Password.new(user.password) == user_params[:password]            
+        valid_password = BCrypt::Password.new(user.password) == user_params[:password]            
         else
             false
         end
