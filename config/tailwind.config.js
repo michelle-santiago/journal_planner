01dd63b94d6440b9a8363ef64e3b16aea1f4c511
Michelle Santiago <mdcsantiago0@gmail.com>
2023-03-26 15:25:48 +0800

    chore: add style to sign in and sign up form

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,10 +8,11 @@ module.exports = {
     './app/views/**/*',
     './node_modules/flowbite/**/*.js' //for flowbite
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+        primary: { "50": "#d1fae5", "100": "#a7f3d0", "200": "#6ee7b7", "300": "#34d399", "400": "#10b981", "500": "#059669", "600": "#047857", "700": "#065f46", "800": "#064e3b", "900": "#064e3b" }
       },
       fontFamily: {
         'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
